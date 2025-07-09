867e1ab5c94da41f88467970c6ed744895f7cfd2
SujeethMuru <muruganandamsujeeth@gmail.com>
2025-07-09 02:44:33 -0500

    Added efficient method from geeksforgeeks for practice

--- Basic/sum of natural numbers.py
+++ Basic/sum of natural numbers.py
@@ -15,6 +15,7 @@ Inputs have to positive
 outputs have to count up to n and add all the number including n
 '''
 # for loop method (naive approach)
+"""
 def sum_of_natural_nums(n):
     sum = 0
     for i in range(1, n+1):
@@ -22,9 +23,27 @@ def sum_of_natural_nums(n):
     return sum
 
 if __name__ == '__main__':
+    n = 3
+    print(f"Output: {sum_of_natural_nums(n)}")
+
     n = 5
     print(f"Output: {sum_of_natural_nums(n)}")
 
+    n = 7
+    print(f"Output: {sum_of_natural_nums(n)}")
+"""
 
+#(Efficient method)
+def sum_of_natural_nums(n):
+    return (n*(n+1))//2
 
+if __name__ == '__main__':
+    n = 3
+    print(f"Output: {sum_of_natural_nums(n)}")
+
+    n = 5
+    print(f"Output: {sum_of_natural_nums(n)}")
+
+    n = 7
+    print(f"Output: {sum_of_natural_nums(n)}")
 
