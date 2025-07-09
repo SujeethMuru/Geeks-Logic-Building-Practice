b1a48429c24c3d71f7930c66f556c8b2736c9f27
SujeethMuru <muruganandamsujeeth@gmail.com>
2025-07-09 02:28:26 -0500

    sum of natural numbers

---
+++ Basic/sum of natural numbers.py
@@ -0,0 +1,30 @@
+'''
+Given a positive integer n, find the sum of the first n natural numbers.
+positive number > 0
+natural number = set of postive numbers
+
+Input n=3
+Output: 6
+Explanation: 1 + 2 + 3 = 6
+
+Input: n = 5
+Output: 15 
+Explanation:  1 + 2 + 3 + 4 + 5 = 15
+
+Inputs have to positive
+outputs have to count up to n and add all the number including n
+'''
+# for loop method (naive approach)
+def sum_of_natural_nums(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
+    return sum
+
+if __name__ == '__main__':
+    n = 5
+    print(f"Output: {sum_of_natural_nums(n)}")
+
+
+
+
