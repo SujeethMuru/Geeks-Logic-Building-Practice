2fb4218b5a0cee1bda00b1d2a5da3eee3289afbd
SujeethMuru <muruganandamsujeeth@gmail.com>
2025-07-16 05:38:12 -0500

    my solution for Find Closest to n and Divisible by m

---
+++ Basic/Find Closest to n and Divisible by m.py
@@ -0,0 +1,64 @@
+#Find Closest to n and Divisible by m
+'''
+Given two integers n and m (m != 0). 
+Find the number closest to n and divisible by m. 
+If there is more than one such number,
+then output the one having maximum absolute value.
+
+Examples: 
+
+Input: n = 13, m = 4
+Output: 12
+Explanation: 12 is the closest to 13, divisible by 4.
+
+Input: n = -15, m = 6
+Output: -18
+Explanation: Both -12 and -18 are closest to -15, 
+but -18 has the maximum absolute value.
+'''
+#MY SOLUTION
+'''
+def find_closest_to_n_divisible_by_m(n, m):
+    list_lesser_n= []
+    list_greater_n = []
+    
+    n_less = n
+    while n_less % m != 0:
+        list_lesser_n.append(n_less-1)
+        n_less -= 1
+    
+    n_greater = n
+    while n_greater % m != 0:
+        list_greater_n.append(n_greater+1)
+        n_greater += 1
+
+    print("Numbers less than n divisible by m: ", list_lesser_n)
+    print("Numbers greater than n divisible by m: ", list_greater_n)
+    print(f"Input: n = {n}, m = {m}")
+
+    return_closest_numbers(list_lesser_n, list_greater_n)
+
+
+def return_closest_numbers(list_lesser, list_greater):
+    if len(list_lesser) < len(list_greater): #lesser number prints
+        print(list_lesser[-1])
+    elif len(list_lesser) == len(list_greater): #if length is equal output maximum absolute value
+        left_pos_num = abs(list_lesser[-1])
+        right_pos_num = abs(list_greater[-1])
+
+        if left_pos_num < right_pos_num:
+            print(right_pos_num)
+        else:
+            print(left_pos_num)
+
+    else: # printer greater number divisble by m
+        print(list_greater[-1])
+        
+    
+if __name__ == "__main__":
+    n = -12
+    m = 6
+
+    find_closest_to_n_divisible_by_m(n, m)
+'''
+
