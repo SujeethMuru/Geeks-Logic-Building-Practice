13d181a7ab5cb42b9e908e6fe3b01a161076fabb
SujeethMuru <muruganandamsujeeth@gmail.com>
2025-07-30 03:32:24 -0500

    Added check power problem, and geeks solutions for square rooting number.

---
+++ Easy/Check if a number is a power of another.py
@@ -0,0 +1,23 @@
+# Check if a number is a power of another number
+
+'''
+Given two positive numbers x and y, 
+check if y is a power of x or not.
+'''
+
+'''
+Examples: 
+
+Input:  x = 10, y = 1
+Output: True
+x^0 = 1
+
+Input:  x = 10, y = 1000
+Output: True
+x^3 = 1
+
+Input:  x = 10, y = 1001
+Output: False
+'''
+
+
--- Medium/Program for Square Root of Integer.py
+++ Medium/Program for Square Root of Integer.py
@@ -31,7 +31,9 @@ def squareRoot(n):
         
 print("Sqaure root: ", squareRoot(16))
 '''
-# My optimized solution:
+
+# My optimized solution: Time Complexity O(1), for large numbers O(log n), Space O(1)
+'''
 def squareRoot(n):
     if n < 0:
         print("Enter Positive number: ")
@@ -39,3 +41,83 @@ def squareRoot(n):
         return int(n ** (1/2))
 
 print("Sqaure root: ", squareRoot(16))
+'''
+
+#Geeks Naive Approach Loop - O(sqrt(n)) Time, O(1) Space
+'''
+def floorSqrt(n):
+
+    #Interate from 1 until square exceeds n
+    res = 1
+    while res * res <= n:
+        res += 1
+
+    # return largest integer whose sqr
+    # is <= n
+    return res - 1
+
+if __name__ == "__main__":
+    n = 11
+    print(floorSqrt(n))
+'''
+
+# Geeks expected approach Binary Search - O(log(n)) Time and O(1) Space
+'''
+def floorSqrt(n):
+
+    #Initial search space
+    lo = 1
+    hi = n
+    res = 1
+
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+
+        # if sqaure of mid is <= n
+        # update result and search upper half
+
+        if mid * mid <= n:
+            res = mid
+            lo = mid + 1
+
+        #If square of mid exceed n,
+        #Search in lower half
+        else:
+            hi = mid - 1
+ 
+    return res
+
+if __name__ == "__main__":
+    n = 11
+    print(floorSqrt(n))
+'''
+
+# Geeks alternate approach Built in functions - O(log(n)) Time, O(1) Space
+'''
+import math
+
+def floorSqrt(n):
+
+    res = int(math.sqrt(n))
+    return res
+
+n = 11
+print(floorSqrt(n))
+'''
+
+#Geek alternate Forumula used by pocket calculators - O(1) Time, O(1) Space
+'''
+import math
+
+def floorSqrt(n):
+
+    res = int(math.exp(0.5 * math.log(n)))
+
+    if (res + 1) ** 2 <= n:
+        res += 1
+    
+    return res
+
+n = 11
+print(floorSqrt(n))
+'''
